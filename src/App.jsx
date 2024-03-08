9183eda11826232f949f5111eebb645d8f2ee3a5
Yevhenii <143257644+Evgeniy-Popov-98@users.noreply.github.com>
2024-03-08 18:07:37 +0200

    add-first-task

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import Profile from './components/Profile/Profile';
+import userData from './components/UserData/UserData';
 
-function App() {
-  return;
-}
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
+};
 
 export default App;
